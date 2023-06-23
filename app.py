ff5d6d567463a686d055eb38c776e36c6afa9dea
scenaristeur <scenaristeur@gmail.com>
2023-06-23 16:41:09 +0200

    messages history

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
 
 
 os.environ['OPENAI_API_KEY'] = apikey
@@ -26,19 +27,25 @@ script_template = PromptTemplate(
     template="ecris-moi le script d'une video Youtube basé sur ce titre {title}"
 )
 
+# Memory
+memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+
 
 # Llms
 llm = OpenAI(temperature=0.9)
 title_chain = LLMChain(llm=llm, prompt=title_template,
-                       verbose=verbose, output_key='title')
+                       verbose=verbose, output_key='title', memory=memory)
 script_chain = LLMChain(llm=llm, prompt=script_template,
-                        verbose=verbose, output_key='script')
+                        verbose=verbose, output_key='script', memory=memory)
 sequential_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=[
                                    'topic'], output_variables=['title', 'script'], verbose=verbose)
 
 
 # show stuff to the screen if there is a prompt
 if prompt:
-    response = sequential_chain({'topic':prompt})
+    response = sequential_chain({'topic': prompt})
     st.write(response['title'])
     st.write(response['script'])
+
+    with st.expander('Historique des messages'):
+        st.info(memory.buffer)
