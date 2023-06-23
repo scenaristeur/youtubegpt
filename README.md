1195e4afd4cc7b014c864cd182b5df54b507f4a7
scenaristeur <scenaristeur@gmail.com>
2023-06-23 16:14:10 +0200

    chain

--- README.md
+++ README.md
@@ -14,3 +14,27 @@ rename apikey_example.py in apikey.py and fill it with your aopen api key
 
 # inspiration
 - tuto https://www.youtube.com/watch?v=MlK6SIjcjE8&t=1288s
+
+# bout de code minimal pour obtenir une réponse 
+`
+# Bring deps
+import os
+from apikey import apikey
+
+import streamlit as st
+from langchain import OpenAI
+
+os.environ['OPENAI_API_KEY'] = apikey
+
+#App framework
+st.title('🦜️🔗 Createur de scripts Youtube')
+prompt = st.text_input('Saisissez votre prompt ici')
+
+# Llms
+llm = OpenAI(temperature= 0.9)
+
+# show stuff to the screen if there is a prompt
+if prompt:
+    response = llm(prompt)
+    st.write(response)
+`
--- app.py
+++ app.py
@@ -3,18 +3,30 @@ import os
 from apikey import apikey
 
 import streamlit as st
-from langchain import OpenAI
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
 
 os.environ['OPENAI_API_KEY'] = apikey
 
-#App framework
+# App framework
 st.title('🦜️🔗 Createur de scripts Youtube')
-prompt = st.text_input('Saisissez votre prompt ici')
+prompt = st.text_input('Saisissez le sujet de votre vidéo Youtube ici')
+
+# Prompt templates
+title_template = PromptTemplate(
+    input_variables=['topic'],
+    template="ecris-moi le titre d'un video Youtube au sujet de {topic}"
+)
+
 
 # Llms
-llm = OpenAI(temperature= 0.9)
+llm = OpenAI(temperature=0.9)
+tittle_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
+
 
 # show stuff to the screen if there is a prompt
 if prompt:
-    response = llm(prompt)
+    response = tittle_chain.run(topic=prompt)
     st.write(response)
